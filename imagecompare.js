eb1d6ca482a91946f4c7dad6a381fc30dee73962
abhijeet.ardale <abhijeet.ardale@digital.hmrc.gov.uk>
2018-02-11 20:42:19 +0000

    new batch implementation

--- imagecompare.js
+++ imagecompare.js
@@ -1,9 +1,9 @@
-const fs = require("mz/fs");
-const path = require('path');
-const recursive = require("recursive-readdir");
-const promiseLimit = require('promise-limit');
-const compareImages = require('resemblejs/compareImages');
-const config = require('./settings.json');
+var fs = require("mz/fs");
+var path = require('path');
+var recursive = require("recursive-readdir");
+var promiseLimit = require('promise-limit');
+var compareImages = require('resemblejs/compareImages');
+var config = require('./settings.json');
 
 const options = {
   output: {
@@ -21,7 +21,7 @@ const options = {
   }
 };
 
-const limit = promiseLimit(25)
+const limit = promiseLimit(5)
 
 const sourceFolder = config.sourceDirectoryPath;
 const destinationFolder = config.testDirectoryPath;
@@ -30,11 +30,23 @@ const configFile = './demo/config.js';
 
 var arrayOfObjects = {'testSuite': 'Visual Regression Test', 'tests': [] }
 
+
+process.env.UV_THREADPOOL_SIZE = 1;
+
+var fs = require('fs');
+var sharp = require('sharp');
+
+sharp.concurrency(1);
+sharp.cache(50);
+
+var maxRss = 0;
+
+
  main();
 
     async function getDiff(file){
-    var startImage = new Date().getTime();
-        //console.log('image comparison started at '+startImage +' ... '+file);
+        var startImage = new Date().getTime();
+        console.log('image comparison started at '+startImage +' ... '+file);
 
         const fileName = path.basename(file);
         const subDir = path.dirname(file).slice(sourceFolder.length) === "" ? "" :  path.dirname(file).slice(sourceFolder.length) +path.sep;
@@ -82,7 +94,7 @@ var arrayOfObjects = {'testSuite': 'Visual Regression Test', 'tests': [] }
                 });
 
         }
-        //console.log('image comparison ended in ' + (new Date().getTime()-startImage) +".  ...."+file);
+        console.log('image comparison ended in ' + (new Date().getTime()-startImage) +".  ...."+file);
         return true;
     }
 
@@ -122,29 +134,253 @@ var arrayOfObjects = {'testSuite': 'Visual Regression Test', 'tests': [] }
 
 
 
-    async function main() {
+    function main() {
         console.log();
         console.log('process started... image analysis inprogress');
         var start = new Date().getTime();
 
-        await fs.writeFile(configFile, arrayOfObjects, {spaces: 2}, function(err) {
+        fs.writeFile(configFile, arrayOfObjects, {spaces: 2}, function(err) {
           if (err) throw err
         });
 
          // ignore files that end in ".css" or  ".html".
-        recursive(sourceFolder, ["*.css", "*.html", "*.xml", "*.xhtml", "*.js"], function (err, files) {
-           if (err) {
-               throw err;
-           }
-           Promise.all(files.map((file) => {
-             return limit(() => getDiff(file))
-           })).then(results => {
-             console.log();
-             console.log('process ended... image analysis finished');
-             console.log('Execution time : ' + Math.round((new Date().getTime()-start)/1000) +"seconds");
-             console.log();
-           });
-        });
+       // recursive(sourceFolder, ["*.css", "*.html", "*.xml", "*.xhtml", "*.js"], function (err, files) {
+         //  if (err) {
+          //     throw err;
+         //  }
+
+           fs.readdirSync(sourceFolder).forEach(function(filename) {
+
+               var input = fs.createReadStream(sourceFolder + filename);
+               var output = fs.createWriteStream(destinationFileName);
+               var transform = sharp().resize(100,100).jpeg({quality : 75, force : false});//sharp().resize(1024, 576).quality(80);
+
+               input.on('error', function(err) {
+                 console.log('input err ' + err);
+               });
+               output.on('error', function(err) {
+                 console.log('output err ' + err);
+               });
+               transform.on('error', function(err) {
+                 console.log('transform err ' + err);
+               });
+
+               output.on('finish', function(err) {
+                 console.dir(sharp.counters());
+                 if (process.memoryUsage().rss > maxRss) {
+                   maxRss = process.memoryUsage().rss;
+                   console.log(maxRss);
+                 }
+               });
+
+               input.pipe(transform).pipe(output);
+
+             });
+
+//batch(files).sequential()
+//.each(function(i, file, done) {
+//         var startImage = new Date().getTime();
+//         var fileName = path.basename(file);
+//         var subDir = path.dirname(file).slice(sourceFolder.length) === "" ? "" :  path.dirname(file).slice(sourceFolder.length) + path.sep;
+//         var destination =  destinationFolder+subDir
+//         var output =  outputFolder+subDir
+//         var destinationFileName =  destination+fileName
+//        fs.readFile(file, function(err, res1){
+//                  if(err){
+//                     console.log(err);
+//                  }
+//                  fs.readFile(destinationFileName, function(err,res2){
+//                      if(err){
+//                         console.log(err);
+//                      }
+//                            setTimeout(() =>
+//                                  require('resemblejs').compare(res1, res2, options, function(err, data) {
+//
+//                                  var fileExists = fs.existsSync(destinationFileName);
+//
+//                                  if(fileExists) {
+//
+//                                     var name = fileName.replace(/\.[^/.]+$/, "");
+//
+//                                      if (!fs.existsSync(output)){
+//                                        fs.mkdirSync(output);
+//                                      }
+//
+//                                      fs.writeFileSync(output+name+'.png', data);
+//
+//                                     // arrayOfObjects.tests.push(createImgConfig(result, file));
+//
+//
+//
+//                                  } else {
+//
+//                                       //arrayOfObjects.tests.push(createImgConfig (null, file))
+//                                  }
+//                                res1=null;
+//                                res2=null;
+//                                data = "4"
+//                                done(data);
+//                              }),500)
+//                           })
+//                       });
+//        }).end(function(results) {
+//           console.log(results)
+//        });
+//           var promises= files.map(function(file){
+//               return new Promise(function(file, resolve, reject){
+//                 var startImage = new Date().getTime();
+//                 console.log('image comparison started at ');
+//                 const fileName = path.basename(file);
+//                 const subDir = path.dirname(file).slice(sourceFolder.length) === "" ? "" :  path.dirname(file).slice(sourceFolder.length) + path.sep;
+//                 const destination =  destinationFolder+subDir
+//                 const output =  outputFolder+subDir
+//                   fs.readFile(file, function(err, res1){
+//                       if(err){
+//                          console.log(err);
+//                       }
+//                       fs.readFile(destination+fileName, function(err, res2){
+//                           if(err){
+//                              console.log(err);
+//                           }
+//                           getDiffData(res1, res2).then((result) => {
+//
+////                                var fileExists = fs.exists(destination+fileName);
+////
+////                                if(fileExists) {
+////
+////                                   var name = fileName.replace(/\.[^/.]+$/, "");
+////
+////                                    if (!fs.exists(output)){
+////                                      fs.mkdir(output);
+////                                    }
+////
+////                                    //fs.writeFileSync(output+name+'.png', data.getBuffer());
+////
+////                                   // arrayOfObjects.tests.push(createImgConfig(result, file));
+////
+////
+////
+////                                } else {
+////
+////                                     //arrayOfObjects.tests.push(createImgConfig (null, file))
+////                                }
+////                                result = null;
+////                                res1 = null;
+////                                res2 = null;
+//                                resolve("3");
+//                          })
+//                       })
+//                   });
+//               }.bind(this, file));
+//           });
+//           Promise.all(promises).then(function(results){
+//               console.log(results);
+//           });
+//           Promise.all(files.map((file) => {
+//             return limit(() => readFile(file))
+//           })).then(results => {
+//             console.log();
+//             console.log('process ended... image analysis finished');
+//             console.log('Execution time : ' + Math.round((new Date().getTime()-start)) +"seconds");
+//             console.log();
+//           });
+        //});
     }
+
+var batch = require('batchflow');
+
+
+
+function readFile(file){
+  return new Promise(function (resolve, reject){
+      var startImage = new Date().getTime();
+      console.log('image comparison started at ');
+      const fileName = path.basename(file);
+      const subDir = path.dirname(file).slice(sourceFolder.length) === "" ? "" :  path.dirname(file).slice(sourceFolder.length) + path.sep;
+      const destination =  destinationFolder+subDir
+      const output =  outputFolder+subDir
+
+      fs.readFile(file).then(
+        res1 =>
+            fs.readFile(destination+fileName).then(
+                 res2 =>
+                    getDiffData(res1, res2).then((result) => {
+
+                        var fileExists = fs.exists(destination+fileName);
+
+                         if(fileExists) {
+
+                            var name = fileName.replace(/\.[^/.]+$/, "");
+
+                             if (!fs.exists(output)){
+                               fs.mkdir(output);
+                             }
+
+                             //fs.writeFileSync(output+name+'.png', data.getBuffer());
+
+                            // arrayOfObjects.tests.push(createImgConfig(result, file));
+
+
+
+                         } else {
+
+                              //arrayOfObjects.tests.push(createImgConfig (null, file))
+                         }
+                         result = null;
+                         res1 = null;
+                         res2 = null;
+                         resolve();
+                   }))).catch(err => console.error(err));
+
+//      fs.readFile(file, function (err, res1){
+//        if (err) reject(err);
+//        else {
+//            fs.readFile(destination+fileName, function (err, res2){
+//                if (err) reject(err);
+//                else {
+//                    getDiffData(res1, res2).then((result) => {
+//
+//                            var fileExists = fs.exists(destination+fileName);
+//
+//                             if(fileExists) {
+//
+//                                var name = fileName.replace(/\.[^/.]+$/, "");
+//
+//                                 if (!fs.exists(output)){
+//                                   fs.mkdir(output);
+//                                 }
+//
+//                                 //fs.writeFileSync(output+name+'.png', data.getBuffer());
+//
+//                                // arrayOfObjects.tests.push(createImgConfig(result, file));
+//
+//
+//
+//                             } else {
+//
+//                                  //arrayOfObjects.tests.push(createImgConfig (null, file))
+//                             }
+//                       }).then(() => { result = null;
+//                                       res1 = null;
+//                                       res2 = null;});
+//                    resolve();
+//                }
+//            });
+//        }
+//      });
+  });
+}
+
+async function getDiffData(file1, file2){
+        var compare = require('resemblejs');
+        resemble.compare(image1, image2, options, function(err, data) {
+              if (err) {
+                reject(err);
+              } else {
+                resolve(data);
+              }
+            });
+        return data.getBuffer();
+}
 
 
